ac5b89a0f5aafe822a22cceef918f6b4efb7c42a
Jacob Weinstock <jakobweinstock@gmail.com>
2021-12-07 19:42:43 -0700

    Add HTTP serving:
    Create initial HTTP listen and serve functions for
    all iPXE binaries.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

---
+++ http.go
@@ -0,0 +1,83 @@
+package ipxe
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"path"
+	"path/filepath"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/go-logr/logr"
+	"github.com/tinkerbell/boots-ipxe/binary"
+	"inet.af/netaddr"
+)
+
+// HandleHTTP is the struct that implements the http.Handler interface.
+type HandleHTTP struct {
+	Log logr.Logger
+}
+
+// ListenAndServeHTTP is a patterned after http.ListenAndServe.
+// It listens on the TCP network address srv.Addr and then
+// calls ServeHTTP to handle requests on incoming connections.
+//
+// ListenAndServeHTTP always returns a non-nil error. After Shutdown or Close,
+// the returned error is http.ErrServerClosed.
+func ListenAndServeHTTP(ctx context.Context, addr netaddr.IPPort, h *http.Server) error {
+	conn, err := net.Listen("tcp", addr.String())
+	if err != nil {
+		return err
+	}
+	return ServeHTTP(ctx, conn, h)
+}
+
+// ServeHTTP is patterned after http.Serve.
+// It accepts incoming connections on the Listener conn and serves them
+// using the Server h.
+//
+// ServeHTTP always returns a non-nil error and closes conn.
+// After Shutdown or Close, the returned error is http.ErrServerClosed.
+func ServeHTTP(_ context.Context, conn net.Listener, h *http.Server) error {
+	return h.Serve(conn)
+}
+
+func trimFirstRune(s string) string {
+	_, i := utf8.DecodeRuneInString(s)
+	return s[i:]
+}
+
+// Handler handles responses to HTTP requests.
+func (s HandleHTTP) Handler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	host, port, _ := net.SplitHostPort(req.RemoteAddr)
+	s.Log = s.Log.WithValues("host", host, "port", port)
+	m := path.Dir(req.URL.Path)
+	if strings.HasPrefix(m, "/") {
+		m = trimFirstRune(path.Dir(req.URL.Path))
+	}
+	// If a mac address is provided, log it. Mac address is optional.
+	// Mac address would be useful for logging and tracing.
+	mac, _ := net.ParseMAC(m)
+	s.Log = s.Log.WithValues("mac", mac)
+
+	got := filepath.Base(req.URL.Path)
+	file, found := binary.Files[got]
+	if !found {
+		s.Log.Info("requested file not found", "file", got)
+		http.NotFound(w, req)
+		return
+	}
+	b, err := w.Write(file)
+	if err != nil {
+		s.Log.Error(err, "error serving file")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	s.Log.Info("file served", "bytes sent", b, "file size", len(file), "file", got)
+	w.WriteHeader(http.StatusOK)
+}
---
+++ http_test.go
@@ -0,0 +1,177 @@
+package ipxe
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-logr/logr"
+	"github.com/google/go-cmp/cmp"
+	"github.com/tinkerbell/boots-ipxe/binary"
+	"inet.af/netaddr"
+)
+
+type fakeResponse struct {
+	headers http.Header
+	body    []byte
+	status  int
+}
+
+func newFakeResponse() *fakeResponse {
+	return &fakeResponse{
+		headers: make(http.Header),
+	}
+}
+
+func (r *fakeResponse) Header() http.Header {
+	return r.headers
+}
+
+func (r *fakeResponse) Write(body []byte) (int, error) {
+	r.body = body
+	return len(body), fmt.Errorf("fake error")
+}
+
+func (r *fakeResponse) WriteHeader(status int) {
+	r.status = status
+}
+
+func (r *fakeResponse) Result() *http.Response {
+	return &http.Response{
+		StatusCode: r.status,
+		Body:       ioutil.NopCloser(bytes.NewBuffer(r.body)),
+	}
+}
+
+func TestListenAndServeHTTP(t *testing.T) {
+	router := http.NewServeMux()
+	s := HandleHTTP{Log: logr.Discard()}
+	router.HandleFunc("/", s.Handler)
+	srv := &http.Server{Handler: router}
+	type args struct {
+		ctx  context.Context
+		addr netaddr.IPPort
+		h    *http.Server
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "fail",
+			args: args{
+				ctx:  context.Background(),
+				addr: netaddr.IPPortFrom(netaddr.IPv4(127, 0, 0, 1), 80),
+				h:    &http.Server{},
+			},
+			wantErr: fmt.Errorf("listen tcp 127.0.0.1:80: bind: permission denied"),
+		},
+		{
+			name: "success",
+			args: args{
+				ctx:  context.Background(),
+				addr: netaddr.IPPortFrom(netaddr.IPv4(127, 0, 0, 1), 9999),
+				h:    srv,
+			},
+			wantErr: fmt.Errorf("http: Server closed"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.args.ctx.Done()
+			tt.args.h.Shutdown(tt.args.ctx)
+			err := ListenAndServeHTTP(tt.args.ctx, tt.args.addr, tt.args.h)
+			if diff := cmp.Diff(err.Error(), tt.wantErr.Error()); diff != "" {
+				t.Fatalf(diff)
+			}
+		})
+	}
+}
+
+func TestHandleHTTP_Handler(t *testing.T) {
+	type req struct {
+		method string
+		url    string
+		body   io.Reader
+	}
+	tests := []struct {
+		name      string
+		req       req
+		want      *http.Response
+		failWrite bool
+	}{
+		{
+			name: "fail",
+			req:  req{method: "POST", url: "/fail"},
+			want: &http.Response{
+				StatusCode: http.StatusMethodNotAllowed,
+			},
+		},
+		{
+			name: "success",
+			req:  req{method: "GET", url: "/snp.efi"},
+			want: &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewBuffer(binary.Files["snp.efi"])),
+			},
+		},
+		{
+			name: "file not found",
+			req:  req{method: "GET", url: "/none.efi"},
+			want: &http.Response{
+				StatusCode: http.StatusNotFound,
+			},
+		},
+		{
+			name: "write failure",
+			req:  req{method: "GET", url: "/snp.efi"},
+			want: &http.Response{
+				StatusCode: http.StatusInternalServerError,
+			},
+			failWrite: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(tt.req.method, tt.req.url, tt.req.body)
+			var resp *http.Response
+			if tt.failWrite {
+				w := newFakeResponse()
+				h := HandleHTTP{Log: logr.Discard()}
+				h.Handler(w, req)
+				resp = w.Result()
+			} else {
+				w := httptest.NewRecorder()
+				h := HandleHTTP{Log: logr.Discard()}
+				h.Handler(w, req)
+				resp = w.Result()
+			}
+
+			defer resp.Body.Close()
+			if diff := cmp.Diff(resp.StatusCode, tt.want.StatusCode); diff != "" {
+				t.Fatalf(diff)
+			}
+			if tt.want.Body != nil {
+				got, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				want, err := ioutil.ReadAll(tt.want.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if diff := cmp.Diff(got, want); diff != "" {
+					t.Fatalf(diff)
+				}
+			}
+		})
+	}
+}
